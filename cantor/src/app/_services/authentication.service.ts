81831c3aeaaa2d09da9332ce0d2d5edfb2a9f3ae
Marcin Chilicki <marcin.chilicki96@gmail.com>
2019-09-26 16:29:52 +0200

    Correct default project names to mine

--- cantor/src/app/_services/authentication.service.ts
+++ cantor/src/app/_services/authentication.service.ts
@@ -20,7 +20,7 @@ export class AuthenticationService {
     }
 
     login(username: string, password: string) {
-        return this.http.post<any>(`/users/authenticate`, { username, password })
+        return this.http.post<User>(`/users/authenticate`, { username, password })
             .pipe(map(user => {
                 // store user details and jwt token in local storage to keep user logged in between page refreshes
                 localStorage.setItem('currentUser', JSON.stringify(user));
